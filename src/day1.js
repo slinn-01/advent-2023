6ae3db1bd2d1666d159fc75cddfab5d9e9210a81
Steve Linn <slinn@anchorgroup.tech>
2023-12-05 12:42:16 -0600

    Update for second problem

--- src/day1.js
+++ src/day1.js
@@ -1,6 +1,17 @@
 // https://adventofcode.com/2023/day/1
 const inputFile = '../input/day1.txt';
 const fs = require('node:fs');
+const numberMap = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+};
 fs.readFile(inputFile, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
@@ -9,20 +20,57 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
     const lines = data.split('\n');
     let sum = 0;
     lines.forEach((line) => {
-        let index = 0;
-        while(isNaN(parseInt(line.charAt(index))) && index < line.length) {
-            index++;
+        let firstDigit;
+        for (let i = 0; i < line.length; i++) {
+            if (!isNaN(parseInt(line.charAt(i)))) {
+                // Character is a number, we can use this as the digit
+                firstDigit = line.charAt(i);
+                break;
+            }
+            const slice3 = line.slice(i, i + 3);
+            if (numberMap[slice3]) {
+                firstDigit = numberMap[slice3];
+                break;
+            }
+            const slice4 = line.slice(i, i + 4);
+            if (numberMap[slice4]) {
+                firstDigit = numberMap[slice4];
+                break;
+            }
+            const slice5 = line.slice(i, i + 5);
+            if (numberMap[slice5]) {
+                firstDigit = numberMap[slice5];
+                break;
+            }
         }
-        let digit = line.charAt(index);
-        if (isNaN(parseInt(line.charAt(index)))) {
+        if (firstDigit === undefined) {
             return;
         }
-        index = line.length - 1;
-        while(isNaN(parseInt(line.charAt(index)))) {
-            index--;
+        let secondDigit;
+        for (let i = line.length - 1; i >= 0; i--) {
+            if (!isNaN(parseInt(line.charAt(i)))) {
+                // Character is a number, we can use this as the digit
+                secondDigit = line.charAt(i);
+                break;
+            }
+            const slice3 = line.slice(i, i + 3);
+            if (numberMap[slice3]) {
+                secondDigit = numberMap[slice3];
+                break;
+            }
+            const slice4 = line.slice(i, i + 4);
+            if (numberMap[slice4]) {
+                secondDigit = numberMap[slice4];
+                break;
+            }
+            const slice5 = line.slice(i, i + 5);
+            if (numberMap[slice5]) {
+                secondDigit = numberMap[slice5];
+                break;
+            }
         }
-        digit += line.charAt(index);
-        sum += parseInt(digit);
+
+        sum += parseInt(`${firstDigit}${secondDigit}`);
     });
     console.log(sum);
 });
